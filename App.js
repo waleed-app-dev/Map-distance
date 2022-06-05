6d742b97e6a4b9945d2c03faec1d61ca20e939db
wHat0 <mapk03@hotmail.com>
2022-06-05 12:49:23 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import FlashMessage from "react-native-flash-message";
 
-export default function App() {
+//Screens
+import ChooseLocation from "./src/Screens/ChooseLocation";
+import Home from "./src/Screens/Home";
+
+const App = () => {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="chooseLocation" component={ChooseLocation} />
+      </Stack.Navigator>
+      <FlashMessage position="top" />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/constants/googleMapKey.js
@@ -0,0 +1,1 @@
+export const GOOGLE_MAP_KEY = "Put_your_key_here";
